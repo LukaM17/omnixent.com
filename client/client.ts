98c570d7f88c6f23635b53c799a3bb329af09a36
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2020-10-20 13:47:45 +0200

    added startSearch method in client.ts

--- client/client.ts
+++ client/client.ts
@@ -1,6 +1,13 @@
 import axios from 'axios'
 
 class OmnixentClient {
+  // search using online server
+  static startSearch (term: string, service: string, lang: string, country: string) {
+    const baseUrl = 'https://api.omnixent.com/api/v1/'
+    const url = `${baseUrl}search?term=${term}&service=${service}&lang=${lang}&country=${country}`
+    return axios.get(url)
+  }
+
   search (term: string, service: string, lang: string, country: string) {
     return axios.get(`${process.env.NUXT_ENV_API_SERVER}search?term=${term}&service=${service}&lang=${lang}&country=${country}`)
   }
